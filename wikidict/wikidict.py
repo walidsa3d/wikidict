b8819eb9670ed7a6f60117b8cc3cc69fd43ba07a
mohamed-aziz <medazizknani@gmail.com>
2016-12-31 15:04:17 +0000

    Using a mutable object to get the thread result.

--- wikidict/wikidict.py
+++ wikidict/wikidict.py
@@ -9,29 +9,25 @@ import os
 import requests
 import argparse
 import textwrap
+from time import sleep
 
-
-class Info:                         # for test purpose
-    SUMMARY = "Not Found"
-
-    def __init__(self):
-        pass
+threads_result = {'mutable': None}
 
 
 def get_summary(query, lang):
-    url = "https://{lang}.wikipedia.org/w/api.php?continue=&action=query&titles={query}&prop=extracts&exintro=&explaintext=&format=json&redirects&formatversion=2".format(
-        lang=lang, query=query)
+    url = "https://{lang}.wikipedia.org/w/api.php?continue=&action=query&titles={query}&prop=extracts&exintro=&explaintext=&format=json&redirects&formatversion=2".format(lang=lang, query=query)
     response = requests.get(url).json()
     pages = response['query']['pages']
-    extract = pages[0].get('extract', None)
-    if extract is not None:
-        Info.SUMMARY = extract
+    threads_result['mutable'] = pages[0].get('extract')
+    return threads_result['mutable']
 
 
 def loading(spinner):
+    stdout.write('Getting the summary pal ')
     stdout.write(spinner.next())
     stdout.flush()
-    stdout.write('\b')
+    stdout.write('\r')
+    sleep(0.08)
 
 
 def main():
@@ -46,14 +42,15 @@ def main():
         name='worker', target=get_summary, args=(query, lang))
     worker.start()
     os.system('setterm -cursor off')
-    spinner = itertools.cycle(['-', '/', '\\'])
+    # returns an infinite iterator
+    spinner = itertools.cycle(["⣾", "⣽", "⣻", "⢿", "⡿", "⣟", "⣯", "⣷"])
     while worker.isAlive():
         loading(spinner)
     terminal_width = os.popen('stty size', 'r').read().split()[1]
     text_width = 50*int(terminal_width)/100
-    summary = colored(Info.SUMMARY, 'green', attrs=['bold'])
+    summary = colored(threads_result['mutable'], 'green', attrs=['bold'])
     if args.color:
         print textwrap.fill(summary, text_width, initial_indent='         ')
     else:
-        print Info.SUMMARY
+        print threads_result['mutable']
     os.system('setterm -cursor on')
